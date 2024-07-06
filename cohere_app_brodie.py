195c4b916497b8d60a05b31ceebf69a59350ce5b
Brodiedeb <97998152+BroDeb@users.noreply.github.com>
2024-07-05 21:22:21 -0400

    New edits

--- cohere_app_brodie.py
+++ cohere_app_brodie.py
@@ -8,6 +8,7 @@ import ast
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts import PromptTemplate
 from langchain_cohere import ChatCohere
+from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.vectorstores import Qdrant
 from langchain_community.embeddings import HuggingFaceEmbeddings
@@ -15,41 +16,6 @@ from langchain_openai import ChatOpenAI
 from streamlit_sortables import sort_items
 
 from qdrant_client import QdrantClient
-
-
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
-model_name="kamalkraj/BioSimCSE-BioLinkBERT-BASE"
-
-@st.cache_resource
-def creating_db(model_name):
-    base_embeddings = HuggingFaceEmbeddings(model_name=model_name)
-    qdrant_client = QdrantClient(
-        url="https://5fff5f2d-b0f0-4ecc-aefa-81905ce94dc9.us-east4-0.gcp.cloud.qdrant.io:6333", 
-        api_key="YYDLXtDyw75MKxpErAvcIqkGPxo_66qZILNb-EDLeoFJPgi8LbdKFQ")
-    qdrant_vectorstore = Qdrant(
-        client=qdrant_client, collection_name="Cardiology ACC guidelines", 
-        embeddings=base_embeddings,
-    )
-    retriever = qdrant_vectorstore.as_retriever(search_type="similarity_score_threshold", search_kwargs={"score_threshold": 0.1})
-    return retriever,qdrant_vectorstore
-
-retriever, qdrant_vectorstore = creating_db(model_name)
-
-cohere_chat_model = ChatCohere(cohere_api_key='GTBBhzYzLyvtESHCSiKSFgcU1QhWHhviGFhVo9Y3')
-
-template = """You are a physician.
-    You will be provided a section of the case scenario and the chief complaint that the patient came in with.
-    Using the chief complaint as a guide, give a ranked list of top differential diagnoses that you can interpret from the section which fit best with the chief complaint.
-    Be as clear and precise as possible. No explanation needed.
-    Format the output as a python list as follows ['Diagnosis 1, Diagnosis 2, Diagnosis 3, ...]'>
-    Here is the section of the scenario {context}"""
-
-
-# Add the context to your user query
-custom_rag_prompt = PromptTemplate.from_template(template)
-
 
 LOGO_IMAGE = "bot-transformed.png"
 
@@ -100,6 +66,62 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+   
+
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
+    st.stop()
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+cohere_chat_model = ChatOpenAI(openai_api_key=openai_api_key,model='gpt-4-turbo')
+# cohere_chat_model = ChatCohere(cohere_api_key='GTBBhzYzLyvtESHCSiKSFgcU1QhWHhviGFhVo9Y3')
+
+model_name="kamalkraj/BioSimCSE-BioLinkBERT-BASE"
+
+@st.cache_resource
+def creating_db(model_name):
+    base_embeddings = HuggingFaceEmbeddings(model_name=model_name)
+    qdrant_client = QdrantClient(
+        url="https://5fff5f2d-b0f0-4ecc-aefa-81905ce94dc9.us-east4-0.gcp.cloud.qdrant.io:6333", 
+        api_key="YYDLXtDyw75MKxpErAvcIqkGPxo_66qZILNb-EDLeoFJPgi8LbdKFQ")
+    qdrant_vectorstore = Qdrant(
+        client=qdrant_client, collection_name="rag_tech_db", 
+        embeddings=base_embeddings,
+    )
+    retriever = qdrant_vectorstore.as_retriever(search_type="similarity_score_threshold", search_kwargs={"score_threshold": 0.1})
+    return retriever,qdrant_vectorstore
+
+retriever, qdrant_vectorstore = creating_db(model_name)
+
+template = """You are a physician.
+    You will be provided a section of the case scenario and the chief complaint that the patient came in with.
+    Using the chief complaint as a guide, give a list of all problems that you can identify .
+    Be as clear and precise as possible. No explanation needed.
+    Format the output as a python list as follows ['Problem 1, Problem 2, Problem 3, ...]'>
+    Here is the section of the scenario {context}"""
+
+
+# Add the context to your user query
+custom_problem_prompt = PromptTemplate.from_template(template)
+
+template = """You are a physician.
+    You will be given a list of problems from a patient case. 
+    Using the list of problems. Using the list, come up with a list of differential diagnoses which fit the best.
+    Arrange those in the order of decreasing likelihood.
+    Be as clear and precise as possible. No explanation needed. 
+    Format the output as a python list as follows ['Diagnosis 1, Diagnosis 2, Diagnosis 3, ...]'>
+    Here are the list of problems {problems}"""
+
+
+# Add the context to your user query
+custom_diag_prompt = PromptTemplate.from_template(template)
+
+
 setting = st.radio(
     "What's setting of this patient encounter?",
     ["Clinic", "ICU", "Floor/wards"],
@@ -136,10 +158,13 @@ elif setting == "ICU":
 else:
     introduction = "You are a physician seeing this patient in the medicine wards."
 
-plan_template = introduction + """The guidelines said, "{context}." Based on what the guidelines said, write an assessment and plan for the problem: {problem} provided \n
+plan_template = introduction + """The guidelines said, "{context}." Here is the scenario: {scenario} Based on what the guidelines said and depending on the scenario, write an assessment and plan for the problem: {problem} provided \n
     The plans should be concrete and specific with what you would do in that situation. The plan should me appropriate for the setting in which you are seeing the patient. \n
     For example, instead of saying start antibiotics, say which antibiotics you would use. \n
-    Be as concise as possible. Format with following structure - Problem: Plan."""
+    Be as concise as possible. \n
+    Think step by step. Its important that you get this right. \n
+    Pay attention to medications that they are on, have allergies to, drug interactions. \n
+    Format with following structure - Problem: Plan."""
 
 
 # Add the context to your user query
@@ -165,6 +190,8 @@ def checkbox_container(data):
         for i in data:
             st.checkbox(i, key='dynamic_checkbox_' + i)
 
+scenario = " ".join([chief_complaint,HPI,Allergies_PMH_PSH_FamilyHx_SocialHx_Meds,HemodynamicMonitoring_FluidBalance_RespiratoryDeviceSettings_PhysicalExamination,LabsAndDiagnostics])
+
 def get_selected_checkboxes():
     return [i.replace('dynamic_checkbox_','') for i in st.session_state.keys() if i.startswith('dynamic_checkbox_') and st.session_state[i]]
 
@@ -174,6 +201,7 @@ if submit_button: # and (chief_complaint is not None and HPI is not None and All
     wt.append(Allergies_PMH_PSH_FamilyHx_SocialHx_Meds)
     wt.append(HemodynamicMonitoring_FluidBalance_RespiratoryDeviceSettings_PhysicalExamination)
     wt.append(LabsAndDiagnostics)
+    
     if len(wt) > len(diagnoses_dictionary):
     	print("Error: The list 'wt' contains more elements than there are keys in the dictionary.")
     else:
@@ -184,21 +212,30 @@ if submit_button: # and (chief_complaint is not None and HPI is not None and All
     # body = {
     #     "prompt": diagnoses_dictionary
     # }
-    rag_chain = (
+    problem_chain = (
         {"context": lambda x: diagnoses_dictionary}
-        | custom_rag_prompt
+        | custom_problem_prompt
         | cohere_chat_model
         | StrOutputParser()
     )
 
-    diagnoses = rag_chain.invoke(diagnoses_dictionary)
+    diagnosis_chain = (
+        {"problems": lambda x: diagnoses_dictionary}
+        | custom_diag_prompt
+        | cohere_chat_model
+        | StrOutputParser()
+    )
+
+
+    problems = st.write_stream(problem_chain.stream(diagnoses_dictionary))
+    diagnoses = diagnosis_chain.invoke(problems)
     st.session_state.diagnoses = diagnoses
     # st.subheader("Output")
     # st.write(st.session_state.diagnoses)
     # out = requests.post("https://xgedf7kieezvkvgdw6qb7igvfe0iznnz.lambda-url.us-west-2.on.aws/", json=body)
     # out = out.json()
     diagnoses_list = st.session_state.diagnoses 
-    diagnoses_list = ast.literal_eval(diagnoses_list)
+    diagnoses_list = ast.literal_eval(str(diagnoses_list))
     st.session_state.diagnoses_list=[item.strip() for item in diagnoses_list]
 
       
@@ -230,16 +267,19 @@ elif st.session_state.diagnoses:
     if generate_plan_button:
 
         plan_rag_chain = (
-        {"context": retriever | format_docs,
-        "problem": RunnablePassthrough()}
+        {"context": RunnablePassthrough(),
+        "problem": RunnablePassthrough(),
+        'scenario':RunnablePassthrough()}
         | plan_rag_prompt
         | cohere_chat_model
         | StrOutputParser()
     )
+           
         for problem in sorted_items:
-            st.write_stream(plan_rag_chain.stream(problem))
-            st.write('Reference:')
             context = qdrant_vectorstore.similarity_search_with_score(problem)
+            retrieved_docs = format_docs(retriever.get_relevant_documents(problem, top_k=5, max_chunk_size=1000))
+            st.write_stream(plan_rag_chain.stream({'context':retrieved_docs, 'problem':problem, 'scenario':scenario}))
+            st.write('Reference:')
             references = ' ; '.join(list(set([i[0].metadata['content'] for i in context])))
             st.write(references)
             st.write(context)
